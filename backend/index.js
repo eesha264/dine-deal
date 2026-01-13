3d35b70f0b1cbc42a82011f01be4033789d00816
Eeshitha Gone <22311A6712@ds.sreenidhi.edu.in>
2026-01-13 16:00:55 +0530

    initial setup

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Dine Deal Backend Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
